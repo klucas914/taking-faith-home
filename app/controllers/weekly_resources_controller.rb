edd1251e3101c4019cbd351c4e2f3d8204ebe14c
Kristin Lucas <klucas914@gmail.com>
2016-11-01 13:59:28 +1000

    updated weekly resources

--- app/controllers/weekly_resources_controller.rb
+++ app/controllers/weekly_resources_controller.rb
@@ -1,5 +1,5 @@
 class WeeklyResourcesController < ApplicationController
-  skip_before_action :authorize
+  skip_before_action :authorize, only: [:index, :creative_response, :memory_verse, :mealtime_prayer, :season_description]
 
   def index
     @weekly_resources = WeeklyResource.all
@@ -14,6 +14,10 @@ class WeeklyResourcesController < ApplicationController
     @weekly_resource = WeeklyResource.new
   end
 
+  def edit
+    @weekly_resource = WeeklyResource.find(params[:id])
+  end
+
   def create
     @weekly_resource = WeeklyResource.new(weekly_resource_params)
  
@@ -21,6 +25,16 @@ class WeeklyResourcesController < ApplicationController
       redirect_to @weekly_resource
     else
       render 'new'
+    end
+  end
+
+  def update
+    @weekly_resource = WeeklyResource.find(params[:id])
+
+    if @weekly_resource.update(weekly_resource_params)
+      redirect_to @weekly_resource
+    else
+      render 'edit'
     end
   end
 
@@ -40,6 +54,13 @@ class WeeklyResourcesController < ApplicationController
   	@weekly_resource = WeeklyResource.find(params[:id])
   end
  
+  def destroy
+    @weekly_resource = WeeklyResource.find(params[:id])
+    @weekly_resource.destroy
+
+    redirect_to weekly_resources_path
+  end
+
   private
   def weekly_resource_params
     params.require(:weekly_resource).permit(:creative_response, :memory_verse, :mealtime_prayer, :season_description)
