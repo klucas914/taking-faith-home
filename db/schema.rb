d663289715233b9d1c66e34beb5a673926166e50
Kristin Lucas <klucas914@gmail.com>
2016-11-03 10:25:46 +1000

    switched database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161101004645) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "readings", force: :cascade do |t|
     t.string   "date"
     t.string   "week"
